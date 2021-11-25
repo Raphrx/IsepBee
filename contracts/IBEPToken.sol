5feb3770e53fe6b2caea0b94f0edfc82b3134340
Raphrx <raphael.raux2@gmail.com>
2021-11-25 11:49:36 +0100

    update

--- contracts/IBEPToken.sol
+++ contracts/IBEPToken.sol
@@ -1,4 +1,4 @@
- pragma solidity ^0.8.0;
+pragma solidity ^0.8.0;
 
 // SPDX-License-Identifier: None
 
@@ -9,28 +9,38 @@ import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract IBEPToken is ERC20, Ownable {
-    
     constructor() public ERC20("IBEP Token", "IBEP") {
-        _mint(_msgSender(), 1_000_000*10**18);
-    }
-    
-    // Warning ! This functions are working without decimals. Do not specify them.
-    
-    function mint(uint256 _amount) onlyOwner public {
-        _mint(_msgSender(), _amount*10**18);
+        _mint(_msgSender(), 1_000_000 * 10**18);
     }
 
     function burn(uint256 amount) public virtual {
-        _burn(_msgSender(), amount*10**18);
+        _burn(_msgSender(), amount * 10**18);
     }
 
-    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
-        uint256 fees = amount/500; // 0,2% fees
-        _transfer(_msgSender(), recipient, (amount - fees));
-        _transfer(_msgSender(), 0x46244c00E6c7a0E25F58398c53DE71dA1f0973F3, fees);
-        return true;
-    }
+    function _transfer(address sender, address recipient, uint256 amount) internal virtual override{
 
-    
-   
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        uint256 fees = amount/500;
+        address poolAddress = 0x46244c00E6c7a0E25F58398c53DE71dA1f0973F3;
+
+        _beforeTokenTransfer(sender, recipient, amount);
+        _beforeTokenTransfer(sender, poolAddress, fees);
+
+        uint256 senderBalance = _balances[sender];
+
+        require(senderBalance >= amount + fees, "ERC20: transfer amount and fees exceeds balance");
+
+        unchecked {
+            _balances[sender] = senderBalance - amount;
+        }
+        _balances[recipient] += amount;
+
+        emit Transfer(sender, recipient, amount);
+        emit Transfer(sender, poolAddress, fees);
+
+        _afterTokenTransfer(sender, recipient, amount);
+        _afterTokenTransfer(sender, poolAddress, fees);
+    }
 }
