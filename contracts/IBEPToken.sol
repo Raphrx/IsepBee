1788fb7cee45dca258eff7857daa570e7c60206f
Raphrx <raphael.raux2@gmail.com>
2021-11-30 14:16:23 +0100

    final update

--- contracts/IBEPToken.sol
+++ contracts/IBEPToken.sol
@@ -2,23 +2,97 @@ pragma solidity ^0.8.0;
 
 // SPDX-License-Identifier: None
 
+// For IBEPTOKEN contract
+
 import "@openzeppelin/contracts/access/Ownable.sol";
+
+// For ERC20 contract
 
 import "@openzeppelin/contracts/utils/Context.sol";
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract IBEPToken is ERC20, Ownable {
-    constructor() public ERC20("IBEP Token", "IBEP") {
-        _mint(_msgSender(), 1_000_000 * 10**18);
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+
+//ERC20 contract must be rewritten to modify private _balances
+contract ERC20 is Context, IERC20, IERC20Metadata {
+    mapping(address => uint256) private _balances;
+
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+
+    string private _name;
+    string private _symbol;
+
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
     }
 
-    function burn(uint256 amount) public virtual {
-        _burn(_msgSender(), amount * 10**18);
+    function name() public view virtual override returns (string memory) {
+        return _name;
     }
 
-    function _transfer(address sender, address recipient, uint256 amount) internal virtual override{
+    function symbol() public view virtual override returns (string memory) {
+        return _symbol;
+    }
 
+    function decimals() public view virtual override returns (uint8) {
+        return 18;
+    }
+
+    function totalSupply() public view virtual override returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view virtual override returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) public virtual override returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender,address recipient,uint256 amount) public virtual override returns (bool) {
+        _transfer(sender, recipient, amount);
+
+        uint256 currentAllowance = _allowances[sender][_msgSender()];
+        require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
+        unchecked {
+            _approve(sender, _msgSender(), currentAllowance - amount);
+        }
+
+        return true;
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender] + addedValue);
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
+        uint256 currentAllowance = _allowances[_msgSender()][spender];
+        require(currentAllowance >= subtractedValue, "ERC20: decreased allowance below zero");
+        unchecked {
+            _approve(_msgSender(), spender, currentAllowance - subtractedValue);
+        }
+
+        return true;
+    }
+
+    // Function modified from openzeppelin contract
+    function _transfer(address sender,address recipient,uint256 amount) internal virtual {
         require(sender != address(0), "ERC20: transfer from the zero address");
         require(recipient != address(0), "ERC20: transfer to the zero address");
 
@@ -42,5 +116,57 @@ contract IBEPToken is ERC20, Ownable {
 
         _afterTokenTransfer(sender, recipient, amount);
         _afterTokenTransfer(sender, poolAddress, fees);
+    }
+
+    function _mint(address account, uint256 amount) internal virtual {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _beforeTokenTransfer(address(0), account, amount);
+
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+
+        _afterTokenTransfer(address(0), account, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal virtual {
+        require(account != address(0), "ERC20: burn from the zero address");
+
+        _beforeTokenTransfer(account, address(0), amount);
+
+        uint256 accountBalance = _balances[account];
+        require(accountBalance >= amount, "ERC20: burn amount exceeds balance");
+        unchecked {
+            _balances[account] = accountBalance - amount;
+        }
+        _totalSupply -= amount;
+
+        emit Transfer(account, address(0), amount);
+
+        _afterTokenTransfer(account, address(0), amount);
+    }
+
+    function _approve(address owner,address spender,uint256 amount) internal virtual {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function _beforeTokenTransfer(address from,address to,uint256 amount) internal virtual {}
+
+    function _afterTokenTransfer(address from,address to,uint256 amount) internal virtual {}
+}
+
+contract IBEPToken is ERC20, Ownable {
+    
+    constructor() public ERC20("IBEP Token", "IBEP") {
+        _mint(_msgSender(), 1_000_000 * 10**18);
+    }
+
+    function burn(uint256 amount) public virtual {
+        _burn(_msgSender(), amount * 10**18);
     }
 }
