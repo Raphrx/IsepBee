16f879c245cd7e797d1c1c79e24e92a109f591de
Raphrx <raphael.raux2@gmail.com>
2021-12-02 11:50:01 +0100

    IBEP Coin

--- contracts/IBEPCoin.sol
+++ contracts/IBEPCoin.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract IBEPToken is ERC20 {
+contract IBEPCoin is ERC20 {
     
     constructor() ERC20("IBEP Coin", "IBEP") {
         _mint(_msgSender(), 1_000_000 * 10**18);
@@ -13,4 +13,3 @@ contract IBEPToken is ERC20 {
     function burn(uint256 amount) public virtual {
         _burn(_msgSender(), amount * 10**18);
     }
-}
